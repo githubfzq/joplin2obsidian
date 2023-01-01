384722468d9e5d7ad933345591d0441f43057b88
githubfzq <u201312526@alumni.hust.edu.cn>
2023-01-01 23:12:47 +0800

    update img attachment with size

--- joplin2obsidian/parser.py
+++ joplin2obsidian/parser.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+from html.parser import HTMLParser
 import re
 
 class MdHandler:
-    src_re = re.compile(r"<img.*src=\"(.*)\".*/>")
+    img_re = re.compile(r"<img.*/>")
 
     def __init__(self, file_path: Path):
         self.file_path = file_path
@@ -11,9 +12,28 @@ class MdHandler:
     def replace_iter(self):
         with self.file_path.open('r') as f:
             for line in f.readlines():
-                yield self.src_re.sub(self._sub_group, line)
+                yield self.img_re.sub(self._sub_group, line)
     
-    def _sub_group(self, matchobj):
-        origin_path = Path(matchobj.group(1))
-        real_path = origin_path.name
-        return f"![[{real_path}]]"
+    def _sub_group(self, matchobj: re.Match):
+        html = matchobj.group(0)
+        parser = ImgParser()
+        parser.feed(html)
+        src = getattr(parser, 'src', None)
+        width = getattr(parser, 'width', None)
+        height = getattr(parser, 'height', None)
+        if src:
+            origin_path = Path(src)
+            real_path = origin_path.name
+            if width and height:
+                return f"![[{real_path}|{width}x{height}]]"
+            if width:
+                return f"![[{real_path}|{width}]]"
+            return f"![[{real_path}]]"
+        return html
+
+
+class ImgParser(HTMLParser):
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
+        if tag == "img":
+            for attr in attrs:
+                setattr(self, *attr)
